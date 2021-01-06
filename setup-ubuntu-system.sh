0b1963d68be54f6f84929a0d554c68b937d6a318
Eric Engle <eengl@users.noreply.github.com>
2021-01-06 10:53:42 -0500

    Adding Ubuntu stuff

---
+++ setup-ubuntu-system.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#set -x
+
+# ----------------------------------------------------------------------------------------
+# Check for Ubuntu
+# ----------------------------------------------------------------------------------------
+if [ -f /etc/os-release ]; then
+   if [[ $(grep '^ID'"=" /etc/os-release) != "ID=ubuntu" ]]; then
+       echo "ERROR: System is Linux, but is not Ubuntu"
+       exit 1
+   fi
+else
+   echo "ERROR: System is not Linux."
+   exit 1
+fi
+
+# ---------------------------------------------------------------------------------------- 
+# Run script only as root
+# ---------------------------------------------------------------------------------------- 
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+   echo "error: Script must be run by root"
+   exit 1
+fi
+
+# ---------------------------------------------------------------------------------------- 
+# Install packages
+# ---------------------------------------------------------------------------------------- 
+apt-get update
+apt-get install $(cat ubuntu/ubuntu-packages.lst)
+
+# ---------------------------------------------------------------------------------------- 
+#  Check if Google Chrome is installed.  If not download and install
+# ---------------------------------------------------------------------------------------- 
+CHROMEURL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+dpkg -s google-chrome-stable > /dev/null
+if [ $? -ne 0 ]; then
+   wget -P /tmp $CHROMEURL
+   dpkg --install /tmp/google-chrome-stable_current_amd64.deb
+fi
+
+# ---------------------------------------------------------------------------------------- 
+# Install 1Password
+# ---------------------------------------------------------------------------------------- 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+add-apt-repository 'deb [arch=amd64] https://onepassword.s3.amazonaws.com/linux/debian edge main'
+apt install 1password
+
+# ---------------------------------------------------------------------------------------- 
+# Set memory paging/swapping settings
+# ---------------------------------------------------------------------------------------- 
+echo "" >> /etc/sysctl.conf
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
---
+++ setup-ubuntu-user.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -x
+
+# ---------------------------------------------------------------------------------------- 
+# Check for Ubuntu
+# ---------------------------------------------------------------------------------------- 
+if [ -f /etc/os-release ]; then
+   if [[ $(grep '^ID'"=" /etc/os-release) != "ID=ubuntu" ]]; then
+       echo "ERROR: System is Linux, but is not Ubuntu"
+       exit 1
+   fi
+else
+   echo "ERROR: System is not Linux."
+   exit 1
+fi
+
+# ---------------------------------------------------------------------------------------- 
+# Make a local bin directory
+# ---------------------------------------------------------------------------------------- 
+mkdir -p $HOME/.local/bin
+
+# ---------------------------------------------------------------------------------------- 
+# Update .bashrc 
+# ---------------------------------------------------------------------------------------- 
+echo "" > /tmp/add_to_bashrc
+echo "if [ -f ~/.bash_profile ]; then" >> /tmp/add_to_bashrc
+echo "   . ~/.bash_profile" >> /tmp/add_to_bashrc
+echo "fi" >> /tmp/add_to_bashrc
+PERMS=$(stat -c "%a" $HOME/.bashrc)
+cat $HOME/.bashrc /tmp/add_to_bashrc > /tmp/new_bashrc
+cp $HOME/.bashrc $HOME/.bashrc.BAK
+cp /tmp/new_bashrc $HOME/.bashrc
+chmod $PERMS $HOME/.bashrc
+
+# ---------------------------------------------------------------------------------------- 
+# Copy in .bash_profile and other hidden config files
+# ---------------------------------------------------------------------------------------- 
+mv $HOME/.profile $HOME/.profile.BAK
+cp ubuntu/dot-bash_profile $HOME/.bash_profile
+ln -s .bash_profile .profile
+cp common/dot-bash_functions $HOME/.bash_functions
+cp common/dot-vimrc $HOME/.vimrc
+mkdir -p $HOME/.vim/swapfiles
+
+# ---------------------------------------------------------------------------------------- 
+# Setup SSH
+# ---------------------------------------------------------------------------------------- 
+mkdir -p $HOME/.ssh/tmp
+cp common/ssh-config $HOME/.ssh/config
+
+# ---------------------------------------------------------------------------------------- 
+# Enable minimize on click
+# ---------------------------------------------------------------------------------------- 
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
