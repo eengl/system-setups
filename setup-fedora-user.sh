8be58d5693897cf68cb399d57e7abde58eb06b0e
Eric Engle <eengl@users.noreply.github.com>
2022-05-10 09:38:55 -0400

    Adding Fedora user setup scripts

---
+++ setup-fedora-user.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -x
+
+# ---------------------------------------------------------------------------------------- 
+# Check for Fedora
+# ---------------------------------------------------------------------------------------- 
+if [ -f /etc/os-release ]; then
+   if [[ $(grep '^ID'"=" /etc/os-release) != "ID=fedora" ]]; then
+       echo "ERROR: System is Linux, but is not Fedora"
+       exit 1
+   fi
+else
+   echo "ERROR: System is not Linux."
+   exit 1
+fi
+
+# ---------------------------------------------------------------------------------------- 
+# Make a local bin directory
+# ---------------------------------------------------------------------------------------- 
+if [ ! -d $HOME/.local/bin ]; then mkdir -p $HOME/.local/bin; fi
+
+# ---------------------------------------------------------------------------------------- 
+# Copy in .bash_profile and other hidden config files
+# ---------------------------------------------------------------------------------------- 
+mv $HOME/.bash_profile $HOME/.bash_profile.ORIG
+cp fedora/dot-bash_profile $HOME/.bash_profile
+ln -s .bash_profile .profile
+cp common/dot-bash_functions $HOME/.bash_functions
+
+# ---------------------------------------------------------------------------------------- 
+# Setup VIM
+# ---------------------------------------------------------------------------------------- 
+cp common/dot-vimrc $HOME/.vimrc
+mkdir -p $HOME/.vim/swapfiles
+
+# ---------------------------------------------------------------------------------------- 
+# Setup SSH
+# ---------------------------------------------------------------------------------------- 
+mkdir -p $HOME/.ssh/tmp
+cp common/ssh-config $HOME/.ssh/config
