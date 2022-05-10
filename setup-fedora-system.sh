50b15e64681d2a808441224d2fca161dcd5916fc
Eric Engle <eengl@users.noreply.github.com>
2022-05-10 12:10:04 -0400

    Update setup-fedora-system.sh
    - fix typo for Google Chrome install
    - Add 1Password install
    - Add update script for root

--- setup-fedora-system.sh
+++ setup-fedora-system.sh
@@ -41,8 +41,15 @@ CHROMEURL="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_6
 rpm -qa google-chrome-stable > /dev/null
 if [ $? -ne 0 ]; then
    wget -P /tmp $CHROMEURL
-   rpm -ivh /tmp/google-chrome-stable_current_x86_64.deb
+   rpm -ivh /tmp/google-chrome-stable_current_x86_64.rpm
 fi
+
+# ---------------------------------------------------------------------------------------- 
+# Install 1Password
+# ---------------------------------------------------------------------------------------- 
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
+dnf install 1password
 
 # ---------------------------------------------------------------------------------------- 
 # Update system crypto policies.
@@ -61,3 +68,12 @@ echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 # ---------------------------------------------------------------------------------------- 
 systemctl enable sshd
 systemctl start sshd
+
+# ---------------------------------------------------------------------------------------- 
+# Update script for root.
+# ---------------------------------------------------------------------------------------- 
+echo "#!/bin/sh" >> $HOME/update
+echo "dnf update" >> $HOME/update
+echo "dnf autoremove" >> $HOME/update
+echo "dnf clean dbcache" >> $HOME/update
+chmod 700 $HOME/update
