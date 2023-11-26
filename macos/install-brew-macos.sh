d4fe85bc2f4cc9d8fd8c8d0012e9f92d11ec408c
Eric Engle <eengl@users.noreply.github.com>
2023-11-26 14:27:25 -0500

    Update install-brew-macos.sh

--- macos/install-brew-macos.sh
+++ macos/install-brew-macos.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]; then
    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-   brew install $(cat brew-macos-packages.txt | grep -v '^#')
+   brew install $(cat homebrew-packages.txt | grep -v '^#')
    #brew cask install opensc
 elif [ "$1" == "uninstall" ]; then
    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
